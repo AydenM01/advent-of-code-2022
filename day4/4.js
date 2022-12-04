5d5db0cbfd3d557b722046bc439251c8bce07d1d
Ayden Marshall <aydenwilliammarshall@gmail.com>
2022-12-04 13:12:40 -0500

    day 4

---
+++ day4/4.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const allFileContents = fs.readFileSync('day4/4-input.txt', 'utf-8');
+
+let sum = 0;
+allFileContents.split(/\r?\n/).forEach(line =>  {
+    const pair = line.split(",");
+    const left = pair[0].split("-");
+    const right = pair[1].split("-");
+    
+    const leftL = parseInt(left[0]);
+    const leftR = parseInt(left[1]);
+    const rightL = parseInt(right[0]);
+    const rightR = parseInt(right[1]);
+    
+    if (leftL <= rightL && leftR >= rightR) {
+        sum++;
+    } else if (rightL <= leftL && rightR >= leftR) {
+        sum++
+    }
+});
+
+console.log(sum);
