3ea042a2cd51ad5d38f90fd521f0789968bad002
Ayden Marshall <aydenwilliammarshall@gmail.com>
2022-12-04 18:24:50 -0500

    Day4 changes

--- day4/4-2.js
+++ day4/4-2.js
@@ -1,23 +1,12 @@
 const fs = require('fs');
 
-const allFileContents = fs.readFileSync('day4/4-input.txt', 'utf-8');
+const isOverlapped = ([left, right]) => {
+    return (left[0] <= right[0] && left[1] >= right[0]) || (right[0] <= left[0] && right[1] >= left[0]);
+}
 
 let sum = 0;
-allFileContents.split(/\r?\n/).forEach(line =>  {
-    const pair = line.split(",");
-    const left = pair[0].split("-");
-    const right = pair[1].split("-");
-    
-    const leftL = parseInt(left[0]);
-    const leftR = parseInt(left[1]);
-    const rightL = parseInt(right[0]);
-    const rightR = parseInt(right[1]);
-    
-    if (leftL <= rightL && leftR >= rightL) {
-        sum++;
-    } else if (rightL <= leftL && rightR >= leftL) {
-        sum++;
-    }
+fs.readFileSync('day4/4-input.txt', 'utf-8').split(/\r?\n/).forEach(line =>  {
+    if (isOverlapped(line.split(",").map(range => range.split("-").map(extrema => parseInt(extrema))))) sum++;
 });
 
 console.log(sum);
